7f34c27718574c52a13e30f2ffd3b4a4df8b07fd
IvanovVasil02 <ivanov.vasil235@gmail.com>
2023-11-13 14:36:32 +0100

    creation of user login record

--- src/main/java/ivanovvasil/u5d5w2Project/payloads/NewDeviceDTO.java
+++ src/main/java/ivanovvasil/u5d5w2Project/payloads/NewDeviceDTO.java
@@ -2,7 +2,7 @@ package ivanovvasil.u5d5w2Project.payloads;
 
 import ivanovvasil.u5d5w2Project.enums.DeviceType;
 import ivanovvasil.u5d5w2Project.validators.ValidEnum;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 
 public record NewDeviceDTO(
 
@@ -10,6 +10,6 @@ public record NewDeviceDTO(
                 message = "The type of device must be one of these:" +
                         " SMARTPHONE, TABLET, LAPTOP")
         String deviceType,
-        @NotNull(message = "The device model is a required field")
+        @NotEmpty(message = "The device model is a required field")
         String model) {
 }
